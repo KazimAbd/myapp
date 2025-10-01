4dd7570cdca84cb980b7d9f6b79d9ddf3edd5151
Kazim <kazimabdullayev70@gmail.com>
2025-10-01 12:10:58 +0400

    v1.0.1 release

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Salam! Bu, CI/CD test application v1.0.1  🚀"
+    return "Salam! Bu, CI/CD test application v1.0.2 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
